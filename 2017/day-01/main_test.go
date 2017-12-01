f3320f1e7bc6bd3d9b110e5e1151976c4aafec20
DULOUT Lionel <ldulout@softwaymedical.fr>
2017-12-01 13:49:18 +0100

    Day 01 - Step 02

--- 2017/day-01/main_test.go
+++ 2017/day-01/main_test.go
@@ -18,3 +18,19 @@ func Test1234(t *testing.T) {
 func Test91212129(t *testing.T) {
 	assert.Equal(t, 9, Captcha("91212129"))
 }
+
+func TestHalf1212(t *testing.T) {
+	assert.Equal(t, 6, CaptchaHalf("1212"))
+}
+func TestHalf1221(t *testing.T) {
+	assert.Equal(t, 0, CaptchaHalf("1221"))
+}
+func TestHalf123425(t *testing.T) {
+	assert.Equal(t, 4, CaptchaHalf("123425"))
+}
+func TestHalf123123(t *testing.T) {
+	assert.Equal(t, 12, CaptchaHalf("123123"))
+}
+func TestHalf12131415(t *testing.T) {
+	assert.Equal(t, 4, CaptchaHalf("12131415"))
+}
