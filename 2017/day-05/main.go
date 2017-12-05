15078f157afb3675fec3bd8096202633ed71abe0
DULOUT Lionel <ldulout@softwaymedical.fr>
2017-12-05 13:28:13 +0100

    Day 05 - Step 02

--- 2017/day-05/main.go
+++ 2017/day-05/main.go
@@ -27,6 +27,19 @@ func (cpu *Cpu) Step() bool {
 	cpu.Current = cpu.Current + instr
 	return true
 }
+func (cpu *Cpu) Step2() bool {
+	if cpu.Current >= len(cpu.Instr) {
+		return false
+	}
+	instr := cpu.Instr[cpu.Current]
+	if instr >= 3 {
+		cpu.Instr[cpu.Current]--
+	} else {
+		cpu.Instr[cpu.Current]++
+	}
+	cpu.Current = cpu.Current + instr
+	return true
+}
 func (cpu *Cpu) Print() string {
 	display := []string{}
 	for i, instr := range cpu.Instr {
@@ -68,9 +81,23 @@ func (m *Maze) CountStepToExit() int {
 	}
 	return count
 }
+func (m *Maze) CountStepToExit2() int {
+	instr := m.readInput()
+	cpu := NewCpu(instr)
+	count := 0
+	// fmt.Printf("%d - %s\n", count, cpu.Print())
+	for cpu.Step2() {
+		count++
+		// fmt.Printf("%d - %s\n", count, cpu.Print())
+	}
+	return count
+}
 
 func main() {
 	input, _ := os.Open("input.txt")
 	maze := Maze{input}
 	fmt.Printf("%d\n", maze.CountStepToExit())
+	input, _ = os.Open("input.txt")
+	maze = Maze{input}
+	fmt.Printf("%d\n", maze.CountStepToExit2())
 }
--- 2017/day-05/main_test.go
+++ 2017/day-05/main_test.go
@@ -42,3 +42,30 @@ func TestFindExitFromMaze(t *testing.T) {
 	maze := Maze{strings.NewReader(input)}
 	assert.Equal(t, 5, maze.CountStepToExit())
 }
+
+func TestOneStep2CanDecrementInstr(t *testing.T) {
+	instr := []int{3, 4, -1, -3, 1}
+	cpu := NewCpu(instr)
+	assert.True(t, cpu.Step2())
+	assert.Equal(t, 3, cpu.Current)
+	assert.Equal(t, []int{2, 4, -1, -3, 1}, cpu.Instr)
+	assert.True(t, cpu.Step2())
+	assert.Equal(t, 0, cpu.Current)
+	assert.Equal(t, []int{2, 4, -1, -2, 1}, cpu.Instr)
+	assert.True(t, cpu.Step2())
+	assert.Equal(t, 2, cpu.Current)
+	assert.Equal(t, []int{3, 4, -1, -2, 1}, cpu.Instr)
+	assert.True(t, cpu.Step2())
+	assert.Equal(t, 1, cpu.Current)
+	assert.Equal(t, []int{3, 4, 0, -2, 1}, cpu.Instr)
+	assert.True(t, cpu.Step2())
+	assert.Equal(t, 5, cpu.Current)
+	assert.Equal(t, []int{3, 3, 0, -2, 1}, cpu.Instr)
+	assert.False(t, cpu.Step2())
+}
+
+func TestFindExitFromMaze2(t *testing.T) {
+	input := "0\n3\n0\n1\n-3\n"
+	maze := Maze{strings.NewReader(input)}
+	assert.Equal(t, 10, maze.CountStepToExit2())
+}
