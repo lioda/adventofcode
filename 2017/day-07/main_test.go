a52847a373b09b08e6901ca57f4381fcd6f3f406
DULOUT Lionel <ldulout@softwaymedical.fr>
2017-12-08 13:12:34 +0100

    Day 07 - Step 02

--- 2017/day-07/main_test.go
+++ 2017/day-07/main_test.go
@@ -8,13 +8,15 @@ import (
 )
 
 func TestLineWithLeaves(t *testing.T) {
-	root, leaves := ParseLine("ddswb (34)")
+	root, weight, leaves := ParseLine("ddswb (34)")
 	assert.Equal(t, "ddswb", root)
+	assert.Equal(t, 34, weight)
 	assert.Empty(t, leaves)
 }
 func TestLineWithHoldings(t *testing.T) {
-	root, leaves := ParseLine("bqyqwn (68) -> wscqe, cwxspl, syogw, xnxudsh")
+	root, weight, leaves := ParseLine("bqyqwn (68) -> wscqe, cwxspl, syogw, xnxudsh")
 	assert.Equal(t, "bqyqwn", root)
+	assert.Equal(t, 68, weight)
 	assert.Equal(t, []string{"wscqe", "cwxspl", "syogw", "xnxudsh"}, leaves)
 }
 func TestFindBottom(t *testing.T) {
@@ -32,5 +34,25 @@ ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)
 `
-	assert.Equal(t, "tknk", FindBottom(strings.NewReader(input)))
+	bottom, _ := FindBottom(strings.NewReader(input))
+	assert.Equal(t, "tknk", bottom)
+}
+
+func TestEquililbrate(t *testing.T) {
+	input := `pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)
+`
+	_, equilibrate := FindBottom(strings.NewReader(input))
+	assert.Equal(t, 60, equilibrate)
 }
