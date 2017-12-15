c0ab616d3adc7358e82807bc94d194f1e5b44af3
DULOUT Lionel <ldulout@softwaymedical.fr>
2017-12-15 13:39:54 +0100

    Day 15 - Step 02

--- 2017/day-15/main.go
+++ 2017/day-15/main.go
@@ -9,13 +9,14 @@ const facB = 48271
 type Generator struct {
 	Factor uint64
 	Last   uint64
+	Mult   uint64
 }
 
 func NewGeneratorA(init uint64) Generator {
-	return Generator{facA, init}
+	return Generator{facA, init, 4}
 }
 func NewGeneratorB(init uint64) Generator {
-	return Generator{facB, init}
+	return Generator{facB, init, 8}
 }
 
 func (g *Generator) Next() string {
@@ -23,6 +24,24 @@ func (g *Generator) Next() string {
 	// fmt.Printf("%d * %d %% %d => %d %% %d => %d\n", g.Last, g.Factor, divisor, g.Last*g.Factor, divisor, result)
 	g.Last = result
 	return fmt.Sprintf("%032b", result)
+}
+func (g *Generator) Chan(max int, c chan string) {
+	for count := 0; count < max; {
+		result := (g.Last * g.Factor) % divisor
+		g.Last = result
+		if result%g.Mult != 0 {
+			continue
+		}
+		count++
+		// fmt.Printf("%d * %d %% %d => %d %% %d => %d\n", g.Last, g.Factor, divisor, g.Last*g.Factor, divisor, result)
+		c <- fmt.Sprintf("%032b", result)
+		// name := "A"
+		// if g.Factor == facB {
+		// 	name = "B"
+		// }
+		// fmt.Printf("%s: %d\n", name, result)
+	}
+	// return fmt.Sprintf("%032b", result)
 }
 
 type Judge struct {
@@ -46,8 +65,38 @@ func (j Judge) Count() int {
 	return result
 }
 
+type Judge2 struct {
+	genA, genB Generator
+}
+
+func (j Judge2) Count() int {
+	max := /*1056*/ 5000000
+	result := 0
+	chanA := make(chan string)
+	chanB := make(chan string)
+	go j.genA.Chan(max, chanA)
+	go j.genB.Chan(max, chanB)
+	for i := 0; i < max; i++ {
+
+		// if i%100000 == 0 {
+		// fmt.Printf("%d (%f%%)\n", i, (float64(i)/float64(max))*100)
+		// }
+		numA := <-chanA
+		numB := <-chanB
+		// numB := j.genB.Next()
+		lowestA := numA[len(numA)-16 : len(numA)]
+		lowestB := numB[len(numB)-16 : len(numB)]
+		// fmt.Printf("%s\n%s\n", lowestA, lowestB)
+		if lowestA == lowestB {
+			result++
+		}
+	}
+	close(chanA)
+	close(chanB)
+	return result
+}
+
 func main() {
-	genA := NewGeneratorA(722)
-	genB := NewGeneratorB(354)
-	fmt.Printf("After 40.000.000, the counter is %d\n", Judge{genA, genB}.Count())
+	fmt.Printf("After 40.000.000, the counter is %d\n", Judge{NewGeneratorA(722), NewGeneratorB(354)}.Count())
+	fmt.Printf("After 5.000.000, the second counter is %d\n", Judge2{NewGeneratorA(722), NewGeneratorB(354)}.Count())
 }
