8fb8a8ac41d80e002f8fbb4a48fc1e77589263e8
Lionel DULOUT <ldulout@softwaymedical.fr>
2018-12-02 17:50:56 +0100

    2018 - day 01 - exo 02

--- 2018/day-01/main.go
+++ 2018/day-01/main.go
@@ -9,13 +9,16 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		panic("a or b ?")
+	}
 	exo := os.Args[1:2][0]
 	input, _ := os.Open("input.txt")
+	changes := readFile(input)
 	if exo == "a" {
-		changes := readFile(input)
 		fmt.Println(applyFrom(0, changes))
 	} else if exo == "b" {
-
+		fmt.Println(findRepeatedFrequency(0, changes))
 	}
 }
 
@@ -35,4 +38,18 @@ func applyFrom(base int, changes []int) int {
 		result += change
 	}
 	return result
+}
+
+func findRepeatedFrequency(base int, changes []int) int {
+	found := make(map[int]bool)
+	found[base] = true
+	frequency := 0
+	for i := 0; ; i++ {
+		frequency += changes[i%len(changes)]
+		if found[frequency] {
+			return frequency
+		} else {
+			found[frequency] = true
+		}
+	}
 }
--- 2018/day-01/main_test.go
+++ 2018/day-01/main_test.go
@@ -17,3 +17,15 @@ func TestWhenApplyTwoValuesThenReturnsResult(t *testing.T) {
 	changes := []int{-5, 4, -9}
 	assert.Equal(t, applyFrom(0, changes), -10)
 }
+
+func TestWhenSearchRepeatedFrequencyThenDoTheLoop(t *testing.T) {
+	changes := []int{1, -2, 3, 1}
+	assert.Equal(t, findRepeatedFrequency(0, changes), 2)
+}
+
+func TestWhenSearchRepeatedFrequencyThenFindIt(t *testing.T) {
+	assert.Equal(t, findRepeatedFrequency(0, []int{1, -1}), 0)
+	assert.Equal(t, findRepeatedFrequency(0, []int{3, 3, 4, -2, -4}), 10)
+	assert.Equal(t, findRepeatedFrequency(0, []int{-6, 3, 8, 5, -6}), 5)
+	assert.Equal(t, findRepeatedFrequency(0, []int{7, 7, -2, -7, -4}), 14)
+}
