a3dc9723a99cab27c93ffd88c0a261ac645a892a
Lionel DULOUT <ldulout@softwaymedical.fr>
2018-12-02 22:13:11 +0100

    2018 - Day 01 - Exo 02 - Rust

--- 2018/day-01/day01/src/main.rs
+++ 2018/day-01/day01/src/main.rs
@@ -3,6 +3,7 @@ use std::io::BufRead;
 use std::io::BufReader;
 use std::fs::File;
 use std::env;
+use std::collections::HashMap;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -22,7 +23,8 @@ fn main() {
         let result = apply_on_0(&changes);
         println!("{}", result);
     } else if mode == "b" {
-        
+        let result = find_first_frequency_appearing_twice(&changes);
+        println!("{}", result);
     }
 }
 
@@ -43,6 +45,28 @@ fn read_changes<'a>(input: &'a mut Read) -> Vec<i32> {
     result
 }
 
+fn find_first_frequency_appearing_twice(changes: &[i32]) -> i32 {
+    // println!("changes to use {:?}", changes);
+    let mut found = HashMap::new();
+    let mut frequency = 0;
+    found.insert(frequency, true);
+    for i in 0..99999999 {
+        let change = changes[i % changes.len()];
+        // println!("frequency {} / {} + {} => {}", i % changes.len(), frequency, change, frequency + change);
+        frequency += change;
+        // if frequency == 394 {
+        //     println!("{} : {:?}", i, frequency);
+        // }else if i % 1000 ==0{
+        //     println!("{} : {:?}", i, frequency);
+        // }
+        if found.contains_key(&frequency)  {
+            return frequency;
+        }
+        found.insert(frequency, true);
+    }
+    0
+}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -56,7 +80,20 @@ mod tests {
     #[test]
     fn test_read_changes() {
         let input = "-96\n+25";
-        // let i = Read.for(input.as_bytes())
         assert_eq!(read_changes(&mut input.as_bytes()), [-96, 25]);
+    }
+
+    #[test]
+    fn test_read_find_first_frequency_appearing_twice_looping() {
+        assert_eq!(find_first_frequency_appearing_twice(&[1, -2, 3, 1]), 2);
+    }
+
+    #[test]
+    fn test_read_find_first_frequency_appearing_twice_all() {
+        assert_eq!(find_first_frequency_appearing_twice(&[1, -1]), 0);
+        assert_eq!(find_first_frequency_appearing_twice(&[3, 3, 4, -2, -4]), 10);
+        assert_eq!(find_first_frequency_appearing_twice(&[-6, 3, 8, 5, -6]), 5);
+        assert_eq!(find_first_frequency_appearing_twice(&[7, 7, -2, -7, -4]), 14);
+
     }
 }
