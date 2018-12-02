c2f2ea6ab918cb3da23185c9acb75f5718941e8a
Lionel DULOUT <ldulout@softwaymedical.fr>
2018-12-02 23:32:38 +0100

    2018 - Day 02 - Exo 02 - Rust

--- 2018/day-02/rust/src/main.rs
+++ 2018/day-02/rust/src/main.rs
@@ -17,7 +17,9 @@ fn main() -> io::Result<()> {
         let result = checksum(&ids);
         println!("{}", result);
     } else if mode == "b" {
-
+        let words = find_words_with_one_difference(&ids);
+        let common = common_str(words.0, words.1);
+        println!("{:?}", common);
     }
 
     Ok(())
@@ -60,6 +62,54 @@ fn has_letter_mult(id: &str) -> (u32, u32) {
     result
 }
 
+pub fn find_words_with_one_difference<'a>(ids: &[&'a str]) -> (&'a str, &'a str) {
+    for curr in 0..ids.len() {
+        let s1 = ids[curr];
+        for i in curr + 1..ids.len() {
+            let s2 = ids[i];
+            if count_differences(s1, s2) == 1 {
+                return (s1, s2);
+            }
+        }
+    }
+    ("", "")
+}
+
+fn count_differences(s1: &str, s2: &str) -> u32 {
+    // println!("count_differences {}/{}", s1, s2);
+    let mut count = 0;
+    // for c1 in s1.chars() {
+    //     for c2 in s2.chars() {
+    //         println!("{}/{}", c1, c2);
+    //         if !c1.eq(&c2) {
+    //             count += 1;
+    //         }
+    //     }
+    // }
+
+    // let chars1 = s1.chars();
+    // let chars2 = s2.chars();
+    for i in 0..s1.len() {
+        if !s1.chars().nth(i).eq(&s2.chars().nth(i)) {
+            // println!("{:?}/{:?}", s1.chars().nth(i), s2.chars().nth(i));
+            count += 1;
+        }
+    }
+    // println!("{}/{} => {}", s1, s2, count);
+    count
+}
+
+pub fn common_str <'a>(s1: &'a str, s2: &'a str) -> String {
+    let mut result = String::new();
+    for i in 0..s1.len() {
+        if s1.chars().nth(i).eq(&s2.chars().nth(i)) {
+            result .push( s1.chars().nth(i).unwrap());
+        }
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -73,4 +123,21 @@ mod tests {
         );
     }
 
+    #[test]
+    fn test_find_words_with_one_difference() {
+        assert_eq!(
+            find_words_with_one_difference(&[
+                "abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"
+            ]),
+            ("fghij", "fguij")
+        );
+    }
+
+    #[test]
+    fn test_common_str() {
+        assert_eq!(
+            common_str("fghij", "fguij"),
+            "fgij"
+        );
+    }
 }
