d7706af545db971395c368ff03cbb8fe1c7643dd
Lioda <lionel.dulout@gmail.com>
2018-12-05 13:42:22 +0100

    Adent 2018 - Day 05 - Exo 02 - Rust

--- 2018/day-05/src/main.rs
+++ 2018/day-05/src/main.rs
@@ -10,15 +10,11 @@ fn main() -> io::Result<()> {
     let mode = &args[1];
 
     let input = fs::read_to_string("src/input.txt")?;
-    // let ids: Vec<&str> = f.split("\n").collect();
 
     if mode == "a" {
-        // let mut polys = (String::from(input), 1);
-        // while polys.1 > 0 {
-        //     polys = reacting_polymers(&polys.0);
-        // }
         println!("{:?}", full_reaction(&input));
     } else if mode == "b" {
+        println!("{:?}", find_shortest_react(&input));
     }
 
     Ok(())
@@ -54,6 +50,38 @@ fn reacting_polymers(input: &str) -> (String, u32) {
     (out, count)
 }
 
+fn remove_and_full_reaction(remove: char, input: &str) -> String {
+    let mut polys = (
+        String::from(input)
+            .replace(&remove.to_lowercase().to_string(), "")
+            .replace(&remove.to_uppercase().to_string(), ""),
+        1,
+    );
+    while polys.1 > 0 {
+        polys = reacting_polymers(&polys.0);
+    }
+    polys.0
+}
+
+fn find_shortest_react(input: &str) -> u32 {
+    unique_chars(input)
+        .iter()
+        .map(|c| remove_and_full_reaction(*c, input))
+        .map(|s| s.len() - 1)
+        .min()
+        .expect("find min") as u32
+}
+
+fn unique_chars(input: &str) -> Vec<char> {
+    let mut vec = input
+        .chars()
+        .map(|c| c.to_ascii_lowercase())
+        .collect::<Vec<char>>();
+    vec.sort();
+    vec.dedup();
+    vec
+}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -73,5 +101,29 @@ mod tests {
     #[test]
     fn test_full_reaction() {
         assert_eq!(full_reaction("dabAcCaCBAcCcaDA\n"), 10);
+    }
+    #[test]
+    fn test_remove_and_full_react() {
+        assert_eq!(
+            remove_and_full_reaction('a', "dabAcCaCBAcCcaDA\n"),
+            String::from("dbCBcD\n")
+        );
+        assert_eq!(
+            remove_and_full_reaction('b', "dabAcCaCBAcCcaDA\n"),
+            String::from("daCAcaDA\n")
+        );
+        assert_eq!(
+            remove_and_full_reaction('c', "dabAcCaCBAcCcaDA\n"),
+            String::from("daDA\n")
+        );
+        assert_eq!(
+            remove_and_full_reaction('d', "dabAcCaCBAcCcaDA\n"),
+            String::from("abCBAc\n")
+        );
+    }
+
+    #[test]
+    fn test_find_shortest_react() {
+        assert_eq!(find_shortest_react("dabAcCaCBAcCcaDA\n"), 4);
     }
 }
