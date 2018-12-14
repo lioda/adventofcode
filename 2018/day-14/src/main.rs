84d04e08fa13637f57babfbe28bead7a9b0ff3d5
Lioda <lionel.dulout@gmail.com>
2018-12-14 18:27:46 +0100

    Advent 2018 - Day 1 - Exo 02 - Rust

--- 2018/day-14/src/main.rs
+++ 2018/day-14/src/main.rs
@@ -4,6 +4,7 @@ use std::fmt;
 use std::fs;
 use std::io;
 use std::io::Write;
+use std::ops::Index;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -18,7 +19,7 @@ fn main() -> io::Result<()> {
     if mode == "a" {
         println!("result : {:?}", exo1(637061));
     } else if mode == "b" {
-        // println!("result : {:?}", exo2(&lines));
+        println!("result : {:?}", exo2("637061", 20000000));
     }
 
     Ok(())
@@ -32,10 +33,13 @@ struct Recipes {
 
 impl Recipes {
     fn new() -> Recipes {
+        let mut vec = Vec::with_capacity(99999);
+        vec.push(3);
+        vec.push(7);
         Recipes {
             elf1: 0,
             elf2: 1,
-            recipes: vec![3, 7],
+            recipes: vec,
         }
     }
 
@@ -43,7 +47,7 @@ impl Recipes {
         self.recipes.len()
     }
 
-    fn next(&mut self) {
+    fn next(&mut self) -> usize {
         let v1 = self.recipes[self.elf1];
         let v2 = self.recipes[self.elf2];
 
@@ -53,24 +57,29 @@ impl Recipes {
             .chars()
             .map(|c| c.to_string().parse::<u32>().expect("atoi"))
             .collect();
+        let result = new_recipes.len();
         self.recipes.append(&mut new_recipes);
 
         self.elf1 = (self.elf1 + (v1 as usize + 1)) % self.recipes.len();
         self.elf2 = (self.elf2 + (v2 as usize + 1)) % self.recipes.len();
+
+        result
     }
 
     fn last_10(&self, index: usize) -> String {
-        // let reverse = self.recipes.clone();
-        // reverse.reverse();
         let last10: Vec<String> = self.recipes[index..index + 10]
             .iter()
             .map(|i| i.to_string())
             .collect();
-        // last10.reverse();
         last10.join("")
+    }
 
-        // .join()
-        // String::new()
+    fn contains(&self, search: &str) -> Option<usize> {
+        self.recipes
+            .iter()
+            .map(|i| i.to_string())
+            .collect::<String>()
+            .find(search)
     }
 }
 
@@ -85,6 +94,29 @@ fn exo1(recipe_num: usize) -> String {
 
     recipes.last_10(recipe_num)
 }
+fn exo2(search: &str, search_at: u32) -> usize {
+    let mut recipes = Recipes::new();
+    let mut recipes_count = 0;
+
+    for breaker in 0..999999999 {
+        recipes_count += recipes.next();
+        if breaker % 100000 == 0 {
+            println!("breaker => {:?}", breaker);
+        }
+
+        if breaker % search_at == 0 {
+            match recipes.contains(search) {
+                None => (),
+                Some(c) => {
+                    recipes_count = c;
+                    break;
+                }
+            }
+        }
+    }
+
+    recipes_count
+}
 
 #[cfg(test)]
 mod tests {
@@ -97,6 +129,14 @@ mod tests {
         assert_eq!(exo1(5), "0124515891");
         assert_eq!(exo1(18), "9251071085");
         assert_eq!(exo1(2018), "5941429882");
+    }
+
+    #[test]
+    fn test_exo2() {
+        assert_eq!(exo2("51589", 100), 9);
+        assert_eq!(exo2("01245", 100), 5);
+        assert_eq!(exo2("92510", 100), 18);
+        assert_eq!(exo2("59414", 100), 2018);
     }
 
     #[test]
@@ -138,6 +178,17 @@ mod tests {
             recipes: vec![3, 7, 1, 0, 1, 0, 1, 2, 4, 8, 6],
         };
         assert_eq!(recipes.last_10(1), "7101012486");
+    }
+
+    #[test]
+    fn test_search() {
+        let recipes = Recipes {
+            elf1: 0,
+            elf2: 6,
+            recipes: vec![3, 7, 1, 0, 1, 0, 1, 2, 4, 8, 6],
+        };
+        assert_eq!(recipes.contains("486"), Some(8));
+        assert_eq!(recipes.contains("777"), None);
     }
 
 }
