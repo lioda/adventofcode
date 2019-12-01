4a04400a0b591eba7b4d5f8f462113b4383512d5
lioda <lionel.dulout@gmail.com>
2019-12-01 12:08:11 +0100

    2019/01 02

--- 2019/day01/src/lib.rs
+++ 2019/day01/src/lib.rs
@@ -5,6 +5,21 @@ pub fn exo01(masses: Vec<i64>)-> i64 {
     fuel
 }
 
+pub fn exo02(masses: Vec<i64>)-> i64 {
+    masses.into_iter().map(|e| {
+        let mut fuel = e / 3 - 2;
+        let mut fuel_for_fuel = fuel;
+        loop {
+            fuel_for_fuel = fuel_for_fuel / 3 - 2;
+            if fuel_for_fuel <= 0 {
+                break;
+            }
+            fuel += fuel_for_fuel;
+        };
+        fuel
+    }).fold(0, |sum, fuel| sum + fuel)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -39,5 +54,21 @@ mod tests {
     #[test]
     fn do_exo01() {
         assert_eq!(exo01(read_input()), 3291760); 
+    }
+
+    #[test]
+    fn it_adds_fuel_for_fuel() {
+        assert_eq!(exo02(vec![1969]), 966); 
+        assert_eq!(exo02(vec![100756]), 50346); 
+    }
+
+    #[test]
+    fn it_adds_modules_and_fuel_for_fuel() {
+        assert_eq!(exo02(vec![1969, 100756]), 50346+966); 
+    }
+
+    #[test]
+    fn do_exo02() {
+        assert_eq!(exo02(read_input()), 4934767); 
     }
 }
