61f2ae274c0a5796af7dda1d06f29bc01bc870ba
Lioda <lionel.dulout@gmail.com>
2020-12-02 13:24:14 +0100

    2020 day 02

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,4 +1,5 @@
 mod exo01;
+mod exo02;
 
 fn main() {
     println!("Hello, world!");
--- 2020/target/rls/debug/deps/aoc_2020-8f11026f8fbed285.d
+++ 2020/target/rls/debug/deps/aoc_2020-8f11026f8fbed285.d
@@ -1,6 +1,7 @@
-/home/lioda/dev/adventofcode/2020/target/rls/debug/deps/aoc_2020-8f11026f8fbed285.rmeta: src/main.rs src/exo01.rs
+/home/lioda/dev/adventofcode/2020/target/rls/debug/deps/aoc_2020-8f11026f8fbed285.rmeta: src/main.rs src/exo01.rs src/exo02.rs
 
-/home/lioda/dev/adventofcode/2020/target/rls/debug/deps/aoc_2020-8f11026f8fbed285.d: src/main.rs src/exo01.rs
+/home/lioda/dev/adventofcode/2020/target/rls/debug/deps/aoc_2020-8f11026f8fbed285.d: src/main.rs src/exo01.rs src/exo02.rs
 
 src/main.rs:
 src/exo01.rs:
+src/exo02.rs:
