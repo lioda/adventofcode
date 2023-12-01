6430628f933ddd2bf81098839fa4246ee6fb1b31
Lioda <lionel.dulout@gmail.com>
2023-12-01 11:51:47 +0100

    feat(2023): step 2

--- 2023/src/01/exo.ts
+++ 2023/src/01/exo.ts
@@ -1,29 +1,83 @@
 import { Lines } from '../exercise/index.js'
 
+const patterns = {
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+}
+
 export class Exo01 {
   public combine(lineReader: Lines): Promise<number> {
     return lineReader.map((s) => this.extractNumber(s)).then((arr) => arr.reduce((a, b) => a + b))
   }
 
   private extractNumber(s: string): number {
-    const digits = this.keepDigit(s)
-    const first = digits.at(0)!
-    const last = digits.at(-1)!
+    const [first, last] = this.keepDigit(s)
 
     return first * 10 + last
   }
 
-  private keepDigit(s: string): number[] {
-    const digits: number[] = []
+  private keepDigit(s: string): [number, number] {
+    let first = 0
+    let last = 0
 
-    for (const c of s) {
-      const potentialDigit = parseInt(c, 10)
-      if (Number.isNaN(potentialDigit)) {
-        continue
+    const calibration = new Calibration(s)
+
+    while (!calibration.over()) {
+      const digit = calibration.extractNextNumber()
+      if (digit) {
+        if (!first) {
+          first = digit
+          last = digit
+        } else {
+          last = digit
+        }
       }
-      digits.push(potentialDigit)
     }
 
-    return digits
+    return [first, last]
+  }
+}
+
+class Calibration {
+  private s: string
+  constructor(originalString: string) {
+    this.s = originalString + ''
+  }
+
+  public over(): boolean {
+    return this.s.length === 0
+  }
+
+  public extractNextNumber(): number | undefined {
+    const found = Object.keys(patterns).find((pattern) => this.s.startsWith(pattern)) as keyof typeof patterns
+    if (!found) {
+      this.shiftOneChar()
+      return undefined
+    }
+
+    const digit = patterns[found as keyof typeof patterns]
+    this.shiftOneChar()
+
+    return digit
+  }
+
+  private shiftOneChar() {
+    this.s = this.s.slice(1)
   }
 }
