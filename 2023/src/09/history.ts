afe44518f33a20cf095453ca9ad8a4b2439c1ff3
Lioda <lionel.dulout@gmail.com>
2023-12-09 17:44:31 +0100

    feat(2023): day09-1

---
+++ 2023/src/09/history.ts
@@ -0,0 +1,43 @@
+export class History {
+  static parse(line: string): History {
+    return new History(line.split(' ').map((s) => parseInt(s)))
+  }
+  constructor(private readonly values: number[]) {}
+
+  public extrapolateNextNumber(): number {
+    const derivations: number[][] = [[...this.values]]
+
+    while (!derivations.at(-1)?.every((n) => n === 0)) {
+      const derivation = this.derivate(derivations.at(-1))
+      derivations.push(derivation)
+    }
+
+    derivations.reverse().map((derivation, i) => {
+      if (i === 0) {
+        derivation.push(0)
+        return derivation
+      }
+      const diff = derivations[i - 1]?.at(-1) ?? 0
+      const toAdd = (derivation.at(-1) ?? 0) + diff
+
+      derivation.push(toAdd)
+      return derivation
+    })
+
+    return derivations.at(-1)?.at(-1) ?? 0
+  }
+
+  private derivate(values?: number[]): number[] {
+    if (!values) throw new Error('WTF')
+
+    const result: number[] = []
+    for (let i = 1; i < values.length; ++i) {
+      const previous = values[i - 1] ?? 0
+      const current = values[i] ?? 0
+
+      const derivated = current - previous
+      result.push(derivated)
+    }
+    return result
+  }
+}
---
+++ 2023/src/09/index.ts
@@ -0,0 +1,14 @@
+import { Solver, TextFileLines } from '../exercise/index.js'
+import { History } from './history.js'
+
+export class DaySolver implements Solver {
+  step01(): unknown {
+    const input = new TextFileLines('src/09/input.txt')
+    return input
+      .map((line) => History.parse(line))
+      .then((histories) => histories.map((history) => history.extrapolateNextNumber()).reduce((a, b) => a + b))
+  }
+  step02(): unknown {
+    throw new Error('Method not implemented.')
+  }
+}
